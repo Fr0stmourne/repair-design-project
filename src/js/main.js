a7e9e1d47d0c5243162df36a758df97711a77629
Frostmourne <cozroman@yandex.ru>
2019-10-07 20:51:26 +0300

    feat: add an iframe and fix some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
+let video = document.querySelector(`#static-video`);
+let playBtn = document.querySelector(`#play-btn`);
+let iframe = document.querySelector(`#iframe`);
+let videoBlock = document.querySelector(`.video`);
 
+
+playBtn.addEventListener(`click`, () => {
+  video.classList.add(`video__video--hide`);
+  playBtn.classList.add(`video__play-btn--hide`);
+  iframe.classList.add(`video__iframe--show`);
+
+});
